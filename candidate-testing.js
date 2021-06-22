69ebdbbe86bc5eeec35ea02d37ae7ad6f129dcb1
michaelshortall <84335397+michaelshortall1@users.noreply.github.com>
2021-06-22 03:16:08 +0000

    Part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -2,6 +2,9 @@ const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
 
+let question  = ["Who was the first American woman in space?", "True or false: 5 kilometer == 5000 meters?", "(5+3)/2*10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?" ];
+
+let answers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 // TODO 1.1a: Define candidateName // 
 let candidateName = "" ;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
@@ -24,19 +27,18 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-   let candidateAnswer = 
-  input.question(question)
-  
+   for (question[0], question[1], question[2], question[3], question[4]) {
+   while (answers[0],answers[1],answers[2], answers[3], answer[4]);
+   }
 
-}
+
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (correctAnswer) {
-console.log("That was the correct answer!");
-}
-}
+
+ let ${answer[0]}/n, ${answer[1]}/n, ${answer[2]}/n, ${answer[3]}/n,${answer[4]}/n,`;
+// Hi John, Doe
 
   let grade;
   
